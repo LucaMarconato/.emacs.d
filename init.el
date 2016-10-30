8b4e8897d6ddaa1d3a6eb9e45a17ca93e3d0b405
LucaMarconato <m.lucalmer@gmail.com>
2016-10-30 14:17:26 +0100

    fix

--- init.el
+++ init.el
@@ -22,21 +22,23 @@
  '(font-latex-fontify-sectioning 1 t)
  '(package-selected-packages
    (quote
-    (company-irony company-irony-c-headers flycheck-irony irony swift-mode auto-complete-c-headers auto-complete company-auctex flycheck-swift yasnippet matlab-mode free-keys flyspell-correct-ivy smartparens shift-text multiple-cursors company-statistics company-shell company-math)))
+    (highlight-symbol company-irony company-irony-c-headers flycheck-irony irony swift-mode auto-complete-c-headers auto-complete company-auctex flycheck-swift yasnippet matlab-mode free-keys flyspell-correct-ivy smartparens shift-text multiple-cursors company-statistics company-shell company-math)))
  '(save-place t nil (saveplace)))
 (let ((default-directory  "~/.emacs.d/lisp/"))
   (normal-top-level-add-subdirs-to-load-path))
 (add-to-list 'exec-path "/usr/local/bin/")
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:background nil))))
- '(font-latex-slide-title-face ((t (:inherit (variable-pitch font-lock-type-face) :height 1 :family "Andale Mono"))))
- '(font-latex-subscript-face ((t nil)))
- '(font-latex-superscript-face ((t nil))))
-(setq-default electric-indent-mode nil)
+;(custom-set-faces
+; ;; custom-set-faces was added by Custom.
+; ;; If you edit it by hand, you could mess it up, so be careful.
+; ;; Your init file should contain only one such instance.
+; ;; If there is more than one, they won't work right.
+; '(default ((t (:background nil))))
+; '(font-latex-slide-title-face ((t (:inherit (variable-pitch font-lock-type-face) :height 1 :family "Andale Mono"))))
+; '(font-latex-subscript-face ((t nil)))
+; '(font-latex-superscript-face ((t nil))))
+(electric-indent-mode 1)
+(electric-pair-mode 1)
+(smartparens-mode 1)
 (xterm-mouse-mode 1)
 
 ;(global-unset-key (kbd "M-f"))
@@ -92,14 +94,43 @@
     )
 (setq ring-bell-function 'ignore)
 (windmove-default-keybindings)
+(subword-mode +1)
+
+(defun universal-argument ()
+  "Begin a numeric argument for the following command.
+Digits or minus sign following \\[universal-argument] make up the numeric argument.
+\\[universal-argument] following the digits or minus sign ends the argument.
+\\[universal-argument] without digits or minus sign provides 4 as argument.
+Repeating \\[universal-argument] without digits or minus sign
+ multiplies the argument by 4 each time.
+For some commands, just \\[universal-argument] by itself serves as a flag
+which is different in effect from any particular numeric argument.
+These commands include \\[set-mark-command] and \\[start-kbd-macro]."
+  (interactive)
+  (setq prefix-arg (list 4))
+  (universal-argument--mode))
+
+(defun my-universal-argument ()
+  (interactive)
+  (setq prefix-arg (list 10))
+  (universal-argument--mode))
+
+(global-set-key (kbd "C-u") 'my-universal-argument)
+
+(global-set-key [(control f7)] 'highlight-symbol)
+(global-set-key [f7] 'highlight-symbol-next)
+(global-set-key [(shift f7)] 'highlight-symbol-prev)
+(global-set-key [(meta f7)] 'highlight-symbol-query-replace)
+
+(add-hook 'fundamental-mode (setq tab-width 4))
 ;--------COMPANY--------
 ;(add-to-list 'company-backends 'company-sourcekit)
 ;(add-to-list 'company-sourcekit)
 (require 'company-sourcekit)
 (add-to-list 'company-backends 'company-sourcekit)
 (add-hook 'swift-mode (lambda () (company-swift-init 1)))
-(setq company-idle-delay 0.07)
-(setq company-dabbrev-downcase 0.07)
+(setq company-idle-delay 0.01)
+(setq company-dabbrev-downcase 0.01)
 ;(setq company-sourcekit-verbose f) ;how to set to false?
 
 ;--------AUTOCOMPLETE--------
@@ -187,6 +218,10 @@
 (setq matlab-shell-command "/Applications/MATLAB_R2016a.app/bin/matlab")
 (setq matlab-shell-command-switches (list "-nodesktop"))
 (matlab-cedet-setup)
+
+;--------NEOTREE--------
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
 
 ;--------LATEX--------
 (if (eq system-type 'darwin)
@@ -349,3 +384,9 @@
 ;
 ;(global-set-key (kbd "C-u") 'my-universal-argument)
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
